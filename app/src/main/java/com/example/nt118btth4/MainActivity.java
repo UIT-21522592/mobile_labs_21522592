79b02457dd9a75dde652084a2bdfba1e86623168
Thắng Nguyễn Trần Hội <21522592@gm.uit.edu.vn>
2023-05-30 20:16:15 +0700

    Done

--- app/src/main/java/com/example/nt118btth4/MainActivity.java
+++ app/src/main/java/com/example/nt118btth4/MainActivity.java
@@ -1,27 +1,84 @@
 package com.example.nt118btth4;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore firestore;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public EditText e1,e2,e3,e4;
+    User User = new User();
+    String in[]={null,null,null,null};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firestore = FirebaseFirestore.getInstance();
-        //Thong tin duoc kiem duyt thi cho vao day
-        Map<String,Object> user = new HashMap<>();
-        user.put("Fullname",);
-        user.put("Phone",);
-        user.put("Username",);
-        user.put("Password",);
+        e1 = (EditText) findViewById(R.id.fullname);
+        e2 = (EditText) findViewById(R.id.phonenumber);
+        e3 = (EditText) findViewById(R.id.username);
+        e4 = (EditText) findViewById(R.id.password);
+
+        in[0] = e1.getText().toString();
+        in[1] = e2.getText().toString();
+        in[2] = e3.getText().toString();
+        in[3] = e4.getText().toString();
+
+        User.checkValidationUserName(in[2]);
+        User.checkValidPhoneNumber(in[1]);
+        User.checkValidationPassword(in[3]);
+
+        User.setUserFullName(in[0]);
+        User.setPhoneNumber(in[1]);
+        User.setUserName(in[2]);
+        User.setUserPassword(in[3]);
+
+        Button clickButton = (Button) findViewById(R.id.button);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Map<String, Object> user = new HashMap<>();
+                user.put("Fullname", User.getUserFullName());
+                user.put("Phonenumber", User.getPhoneNumber());
+                user.put("Username", User.getUserName());
+                user.put("Password", User.getUserPassword());
+
+                // Add a new document with a generated ID
+                db.collection("Khach Hang")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/nt118btth4/User.java
+++ app/src/main/java/com/example/nt118btth4/User.java
@@ -1,15 +1,17 @@
 package com.example.nt118btth4;
 
+import android.widget.Toast;
+
 public class User {
     private String userFullName;
-    private int phoneNumber;
+    private String phoneNumber;
     private String userName;
     private String userPassword;
-
     public User(){
         this.userFullName = null;
-        this.phoneNumber = 0;
+        this.phoneNumber = null;
         this.userName = null;
+        this.userPassword = null;
     }
     public void setUserFullName(String in1){ this.userFullName = in1;}
     public String getUserFullName(){return this.userFullName;}
@@ -22,16 +24,59 @@ public class User {
         this.userName = userName;
     }
 
-    public int getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public void checkValidationUserName(String in){
-        if(in.length() >= 6)
+    public void setUserPassword(String userPassword){this.userPassword = userPassword;}
+
+    public String getUserPassword(){return userPassword;}
+    public boolean checkValidationUserName(String in){
+        if(in.length() >= 6){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkValidPhoneNumber(String in){
+        if(in.length() <= 10){
+            for(int i = 0; i<in.length();i++) {
+                if(Character.isDigit(in.charAt(i))){
+                    continue;
+                }else return false;
+            }
+        }
+        return false;
+    }
+
+// Quy uoc:
+//    -1 la khong ton tai
+//    -2 la khong co ky tu dac biet
+//    -3 la khong co chu viet hoa
+//    -4 la khong co so
+    public int checkValidationPassword(String in){
+        if(in.isEmpty() == false){
+            if(in.length() >= 6){
+
+                boolean flag[] = {false,false,false};
+                for(int i=0;i<in.length();i++){
+                    if(!Character.isLetterOrDigit(in.charAt(i))) flag[0] = true; // Ham kiem tra xem co ky tu dac biet khong
+                    if(Character.isUpperCase(in.charAt(i))) flag[1] = true; // Ham kiem tra xem co ky tu viet hoa
+                    if(Character.isDigit(in.charAt(i))) flag[0] = true; // Ham kiem tra xem co so khong
+                }
+                for(int i=0;i<3;i++){
+                    if(flag[i] == false){
+                        return 0-i-1;
+                    }
+                }
+            }
+        }
+
+        return -1;
     }
 
 }
